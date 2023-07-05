10e7c00c891adabf3fa020de38cae0a69a80155d
Mirobid Abdullayev <80286890+MirobidAbdullayev@users.noreply.github.com>
2023-07-05 22:27:08 +0500

    function, route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware'=>'guest'],function(){
+    Route::get('login',[HomeController::class,'index'])->name('login');
+    Route::post('login',[HomeController::class,'login'])->name('login')->middleware('throttle:2,1');
+
+    Route::get('register',[HomeController::class,'register_view'])->name('register');
+    Route::post('register',[HomeController::class,'register'])->name('register')->middleware('throttle:2,1');
+});
+Route::group(['middleware'=>'auth'],function(){
+    Route::get('redirect',[HomeController::class,'redirect'])->name('redirect');
+    Route::get('logout',[HomeController::class,'logout'])->name('logout');
 });
