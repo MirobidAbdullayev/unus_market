e02a681c36ca9a6e7cd2d805d82c1de40e9eb539
Mirobid Abdullayev <80286890+MirobidAbdullayev@users.noreply.github.com>
2023-07-10 23:09:42 +0500

    admin panel

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    @include('admin.css')
+  <body>
+    @include('admin.sidebar')
+      
+    @include('admin.header')
+        
+    @include('admin.body')
+    
+    @include('admin.script')
+  </body>
+  </head>
+</html>
